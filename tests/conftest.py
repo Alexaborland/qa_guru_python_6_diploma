28728292d242bdb4b98f46ee41d96eb59d4e3334
Aleksandra Kashina <Borland3711@gmail.com>
2023-10-08 00:07:08 +0300

    Changed conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,34 @@
 import pytest
 from selene.support.shared import browser
+from utils import allure_attach
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
 
 
-@pytest.fixture(scope="function", autouse=True)
+@pytest.fixture(scope='function', autouse=True)
 def open_browser():
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options)
+
+    browser.config.driver = driver
+    browser.config.window_width = 1200
+    browser.config.window_height = 800
     browser.config.base_url = 'https://demowebshop.tricentis.com/'
 
-    yield
-
+    yield browser
+    allure_attach.add_html(browser)
+    allure_attach.add_screenshot(browser)
+    allure_attach.add_logs(browser)
+    allure_attach.add_video(browser)
     browser.quit()
